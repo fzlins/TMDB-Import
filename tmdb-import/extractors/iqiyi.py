c1f624b0aaf64288f2c3d406bfffa939caf39be4
fzlins <fzlins2013@gmail.com>
2024-05-16 09:20:35 -0400

    fix(iqiyi): iqiyi_extractor

--- tmdb-import/extractors/iqiyi.py
+++ tmdb-import/extractors/iqiyi.py
@@ -10,9 +10,9 @@ def iqiyi_extractor(url):
     logging.info("iqiyi_extractor is called")
     webPage = open_url(url)
     if url.__contains__("iqiyi.com/lib/m_"):
-        albumId = re.search(r'movlibalbumaid=\"(.*?)\"', str(webPage)).group(1)
+        albumId = re.search(r'movlibalbumaid=\"(\d+)\"', str(webPage)).group(1)
     else:
-        albumId = re.search(r'\"albumId\":(.*?),\"channelId', str(webPage)).group(1)
+        albumId = re.search(r'\"albumId\":(\d+),\"channelId', str(webPage)).group(1)
     logging.info(f"album_id: {albumId}")
     apiRequest = f"https://pcw-api.iqiyi.com/album/album/baseinfo/{albumId}"
     logging.debug(f"API request: {apiRequest}")
