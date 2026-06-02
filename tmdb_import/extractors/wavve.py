b1dd271c0e6afa6f9e0df54224ef7f0a70637eb1
fzlins <fzlins2013@gmail.com>
2026-06-02 10:38:12 -0400

    fix(wavve): handle pagination with offset

--- tmdb_import/extractors/wavve.py
+++ tmdb_import/extractors/wavve.py
@@ -9,17 +9,27 @@ from ..common import Episode, Metadata, Season, open_url
 def wavve_extractor(url):
     logging.info("wavve_extractor is called")
     programid = re.search(r'programid=(.*?)[\&$]', str(url)).group(1)
-    apiRequest = f"https://apis.wavve.com/fz/vod/programs-contents/{programid}?limit=50&offset=0&orderby=old&apikey=E5F3E0D30947AA5440556471321BB6D9&credential=none&device=pc&drm=wm&partner=pooq&pooqzone=none&region=kor&targetage=all"
-    logging.debug(f"API request url: {apiRequest}")
-    sourceData = json.loads(open_url(apiRequest))
     episodes = {}
     episode_number = 1
-    for episode in sourceData["cell_toplist"]["celllist"]:
-        episode_name = episode["title_list"][0]["text"]
-        episode_air_date = episode["title_list"][1]["text"].rsplit(' ', 1)[-1].split("(")[0]
-        episode_runtime = round(int(episode["_playtime_log"].split(',')[0])/60)
-        episode_overview = episode["synopsis"]
-        episode_backdrop = f"https://{episode['thumbnail']}"
-        episodes[episode_number] = Episode(episode_number, episode_name, episode_air_date, episode_runtime, episode_overview, episode_backdrop)
-        episode_number = episode_number + 1 
+    offset = 0
+    while True:
+        apiRequest = f"https://apis.wavve.com/fz/vod/programs-contents/{programid}?limit=50&offset={offset}&orderby=old&apikey=E5F3E0D30947AA5440556471321BB6D9&credential=none&device=pc&drm=wm&partner=pooq&pooqzone=none&region=kor&targetage=all"
+        logging.debug(f"API request url: {apiRequest}")
+        sourceData = json.loads(open_url(apiRequest))
+        for episode in sourceData["cell_toplist"]["celllist"]:
+            episode_name = episode["title_list"][0]["text"]
+            episode_air_date = episode["title_list"][1]["text"].rsplit(' ', 1)[-1].split("(")[0]
+            episode_runtime = round(int(episode["_playtime_log"].split(',')[0])/60)
+            episode_overview = episode["synopsis"]
+            episode_backdrop = f"https://{episode['thumbnail']}"
+            episodes[episode_number] = Episode(episode_number, episode_name, episode_air_date, episode_runtime, episode_overview, episode_backdrop)
+            episode_number = episode_number + 1
+
+        if len(sourceData["cell_toplist"]["celllist"]) == 0:
+            break
+
+        if int(sourceData["cell_toplist"]["pagecount"]) > int(sourceData["cell_toplist"]["count"]):
+            offset = offset + 50
+        else:
+            break
     return Metadata(url=url, id=programid, language="ko-KR", seasons=[Season(None, episodes=episodes)])
